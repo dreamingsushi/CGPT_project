07774cbd56f52e569aef14d6bc50fe7dc8a036e2
dreamingsushi <130603426+dreamingsushi@users.noreply.github.com>
2023-09-18 22:02:08 +0800

    A

---
+++ DARKSOUL/Assets/thirdpersonmovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thirdpersonmovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+    [SerializeField] private float speed;
+
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            // Atan2 is a mathematical function that returns the angle between x-axis and the vector starting at zero, and terminating at x comma y.
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y , targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle , 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+    }
+}
